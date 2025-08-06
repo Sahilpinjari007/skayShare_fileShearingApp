31a67a7031c9f2946135608165e5763a6d7d2237
Sahilpinjari007 <sahilpinjari51209@gmail.com>
2025-08-06 13:14:20 +0530

    Initial commit for new version

--- src/components/Login.jsx
+++
@@ -1,8 +0,0 @@
-import { SignIn } from '@clerk/clerk-react'
-import React from 'react'
-
-function Login() {
-  return <SignIn forceRedirectUrl={"/upload"} signUpUrl={'/auth/register'}/>
-}
-
-export default Login
